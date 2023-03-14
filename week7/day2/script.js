fd19826670a63873ea0a51c901b5811920a7db06
Massolinii <massimilianoesposito548@gmail.com>
2023-03-14 16:34:28 +0100

    daily

---
+++ week7/day2/script.js
@@ -0,0 +1,43 @@
+/* Esercizio 1 */
+const saveButton = document.getElementById("save");
+const removeButton = document.getElementById("remove");
+const inputField = document.getElementById("regName");
+const personSpan = document.getElementById("person");
+
+const savedName = localStorage.getItem("name");
+if (savedName) {
+  personSpan.textContent = savedName;
+}
+
+saveButton.addEventListener("click", function () {
+  let name = inputField.value.trim();
+  if (name) {
+    localStorage.setItem("name", name);
+    personSpan.textContent = name;
+  }
+  inputField.value = "";
+});
+
+removeButton.addEventListener("click", function () {
+  localStorage.removeItem("name");
+  personSpan.textContent = "";
+  inputField.value = "";
+});
+
+/* Esercizio 2 */
+
+if (sessionStorage.getItem("timePassed")) {
+    var timePassed = sessionStorage.getItem("timePassed");
+  } else {
+    var timePassed = 0;
+  }
+
+  var counterElement = document.getElementById("counter");
+
+  function addTime() {
+    timePassed++;
+    sessionStorage.setItem("timePassed", timePassed);
+    counterElement.textContent = timePassed;
+  }
+
+  setInterval(addTime, 1000);
