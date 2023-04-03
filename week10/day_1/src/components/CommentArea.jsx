2c9b0ee9c3a3ac04ac1bb5f5d7ae0817381fa717
Massolinii <massimilianoesposito548@gmail.com>
2023-04-03 17:02:46 +0200

    nworking

---
+++ week10/day_1/src/components/CommentArea.jsx
@@ -0,0 +1,54 @@
+import { Component } from "react";
+import ListGroup from "react-bootstrap/ListGroup";
+
+class CommentArea extends Component {
+  state = {
+    comments : [],
+  }
+  
+
+
+  myAuth =
+"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDJhY2RmNzY4MzQzMTAwMTRkZWE3ODYiLCJpYXQiOjE2ODA1MjY4NjMsImV4cCI6MTY4MTczNjQ2M30.FDfgzJ_h12manMK6eSxbniE_d-EdSNZD7Vt6OkDqhKs"
+  myUrl = "https://striveschool-api.herokuapp.com/api/comments/";
+
+  getComments = async (elementId) => {
+    try {
+      let response = await fetch(`${this.myUrl}${elementId}`, {
+        headers: {
+          Authorization: this.myAuth,
+        },
+      });
+      if (response.ok) {
+        let myComments = await response.json();
+        if (myComments) {        
+          
+          this.setState({ comments: myComments }, () => { console.log(this.state.comments) })
+            
+          console.log(this.state.comments);
+          console.log(myComments)
+        }
+      } else {
+        console.log("ERROR : Something went wrong in the API call");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  async componentDidMount() {
+    await this.getComments(this.props.bookAsin);
+  }
+
+  render() {
+    return (
+      <ListGroup>
+        {this.state.comments.map((comment) => {
+            <ListGroup.Item key={comment._id}>{comment.comment}Prova</ListGroup.Item>
+        })}
+      </ListGroup>
+    );
+  }
+}
+
+export default CommentArea;
