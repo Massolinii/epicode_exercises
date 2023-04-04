215f5168759cbef12d977c9fabe1ff6b618c5d38
Massolinii <massimilianoesposito548@gmail.com>
2023-04-04 17:00:29 +0200

    class to functions

--- week10/day_1/src/components/AddComment.jsx
+++ week10/day_1/src/components/AddComment.jsx
@@ -1,16 +1,13 @@
-import { Component } from "react";
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 const API_URL = "https://striveschool-api.herokuapp.com/api/comments/";
 
-class AddComment extends Component {
-  state = {
-    comment: "",
-    rate: 5,
-    elementId: this.props.asin,
-  };
+const AddComment = ({ asin }) => {
+  const [comment, setComment] = useState("");
+  const [rate, setRate] = useState(5);
 
-  postComment = async (e) => {
+  const postComment = async (e) => {
     e.preventDefault();
     try {
       let response = await fetch(API_URL, {
@@ -20,17 +17,12 @@ class AddComment extends Component {
           Authorization:
             "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDJhZDA4YjY4MzQzMTAwMTRkZWE3OGQiLCJpYXQiOjE2ODA1Mjc0OTksImV4cCI6MTY4MTczNzA5OX0.Zg-_qRAESiz64rLknV3mP89OJYAHFfGo8EY6TkaugrU",
         },
-        body: JSON.stringify(this.state),
+        body: JSON.stringify({ comment, rate, elementId: asin }),
       });
       if (response.ok) {
         alert("Comment successfully sent!");
-        this.setState({
-          comment: {
-            comment: "",
-            rate: 1,
-            elementId: this.props.asin,
-          },
-        });
+        setComment("");
+        setRate(5);
       } else {
         console.log("error");
         alert("Comment was NOT sent.");
@@ -41,36 +33,34 @@ class AddComment extends Component {
     }
   };
 
-  render() {
-    return (
-      <Form onSubmit={(e) => this.postComment(e)}>
-        <Form.Group className="my-3">
-          <Form.Label>Comment :</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter your comment"
-            value={this.state.comment}
-            onChange={(e) => this.setState({ comment: e.target.value })}
-          />
-        </Form.Group>
+  return (
+    <Form onSubmit={postComment}>
+      <Form.Group className="my-3">
+        <Form.Label>Comment :</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter your comment"
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+        />
+      </Form.Group>
 
-        <Form.Group className="my-3">
-          <Form.Label>Rating</Form.Label>
-          <Form.Control
-            type="number"
-            min={0}
-            max={5}
-            placeholder="0 - 5"
-            onChange={(e) => this.setState({ rate: e.target.value })}
-            value={this.state.rate}
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Comment
-        </Button>
-      </Form>
-    );
-  }
-}
+      <Form.Group className="my-3">
+        <Form.Label>Rating</Form.Label>
+        <Form.Control
+          type="number"
+          min={0}
+          max={5}
+          placeholder="0 - 5"
+          onChange={(e) => setRate(e.target.value)}
+          value={rate}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Comment
+      </Button>
+    </Form>
+  );
+};
 
 export default AddComment;
--- week10/day_1/src/components/CommentArea.jsx
+++ week10/day_1/src/components/CommentArea.jsx
@@ -1,61 +1,50 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import ListGroup from "react-bootstrap/ListGroup";
 import AddComment from "./AddComment";
 
-class CommentArea extends Component {
-  state = {
-    comments: [],
-  };
+const CommentArea = ({ bookAsin }) => {
+  const [comments, setComments] = useState([]);
 
-  myAuth =
+  const myAuth =
     "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDJhY2RmNzY4MzQzMTAwMTRkZWE3ODYiLCJpYXQiOjE2ODA1MjY4NjMsImV4cCI6MTY4MTczNjQ2M30.FDfgzJ_h12manMK6eSxbniE_d-EdSNZD7Vt6OkDqhKs";
-  myUrl = "https://striveschool-api.herokuapp.com/api/comments/";
+  const myUrl = "https://striveschool-api.herokuapp.com/api/comments/";
 
-  getComments = async (elementId) => {
+  const getComments = async (elementId) => {
     try {
-      let response = await fetch(`${this.myUrl}${elementId}`, {
+      const response = await fetch(`${myUrl}${elementId}`, {
         headers: {
-          Authorization: this.myAuth,
+          Authorization: myAuth,
         },
       });
       if (response.ok) {
-        let myComments = await response.json();
+        const myComments = await response.json();
         if (myComments) {
-          this.setState({ comments: myComments }, () => {
-            console.log(this.state.comments);
-          });
-
-          console.log(this.state.comments);
-          console.log(myComments);
+          setComments(myComments);
         }
       } else {
-        console.log("ERROR : Something went wrong in the API call");
+        console.log("ERROR: Something went wrong in the API call");
       }
     } catch (error) {
       console.log(error);
     }
   };
 
-  async componentDidMount() {
-    await this.getComments(this.props.bookAsin);
-  }
+  useEffect(() => {
+    getComments(bookAsin);
+  }, [bookAsin]);
 
-  render() {
-    return (
-      <div>
-        <ListGroup>
-          {this.state.comments.map((comment) => {
-            return (
-              <ListGroup.Item key={comment._id}>
-                {comment.comment}
-              </ListGroup.Item>
-            );
-          })}
-        </ListGroup>
-        <AddComment />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <ListGroup>
+        {comments.map((comment) => {
+          return (
+            <ListGroup.Item key={comment._id}>{comment.comment}</ListGroup.Item>
+          );
+        })}
+      </ListGroup>
+      <AddComment setComments={setComments} bookAsin={bookAsin} />
+    </div>
+  );
+};
 
 export default CommentArea;
